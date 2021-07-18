2368d7587bc97bfb502ce1c8383593f10211dc0d
Muhammad Thouseef <muhammadthouseef05@gmail.com>
2021-07-18 05:53:55 +0530

    added sitemap

--- Desktop/quick akbar/cron/main_code_v2.py
+++ Desktop/quick akbar/cron/main_code_v2.py
@@ -25,6 +25,9 @@ from PIL import Image
 import io
 import hashlib 
 import json
+import xml.etree.ElementTree as ET
+from datetime import datetime
+import requests
 
 
 try:
@@ -33,7 +36,8 @@ except:
     #cred = credentials.Certificate(os.getcwd()+'/cron/quickakhbar20-firebase-adminsdk-6oacd-661156e85b.json')
     #cred = credentials.Certificate('/home/ubuntu/quickakhbar20-firebase-adminsdk-6oacd-661156e85b.json')
     cred = credentials.Certificate('/home/dev/Downloads/quickakhbar20-firebase-adminsdk-6oacd-661156e85b.json')
-    default_app = firebase_admin.initialize_app(cred,{'databaseURL': 'https://quickakhbar20.firebaseio.com'})
+    default_app = firebase_admin.initialize_app(cred,{'databaseURL': 'https://quickakhbar20.firebaseio.com','storageBucket':'quickakhbar20.appspot.com'})
+    #default_storage_app = firebase_admin.initialize_app(cred,{'storageBucket':'https://quickakhbar20.appspot.com'})
 
 bucket = storage.bucket("quickakhbar20.appspot.com")
 size = 512,512
@@ -47,7 +51,7 @@ def get_page_content(link):
 
 def parse_news(link,data):
     try:
-        import pdb;pdb.set_trace()
+        #import pdb;pdb.set_trace()
         stop_words = set(stopwords.words(data["language"]))
         db = firestore.client()
         transaction = db.transaction()
@@ -72,18 +76,24 @@ def parse_news(link,data):
             return
         dictnry = {id_:{"title":head_lines,"domain":data["domain"],"url":link,"image_url":com_image,"date":source_date,
                     "time":published_time,"summary":summary,"live_date":live_date,"contry":data["country"],"key":key,"category":data["category"]}}
+        country, lang = data["country"].split("/")
+        site_map_dict = {"key":id_, "country":country,"language": lang}
         #users_ref = ref.child(''.join(filter(str.isalnum, id_)))
         #users_ref.set(dictnry)
+        print("bbb")
+        import pdb;pdb.set_trace()
         aaa = update_in_transaction(transaction, ref,dictnry)
         print("aaa")
-        return dictnry
+        return dictnry,site_map_dict
     except:
         #logger.error("something in xpath happened", exc_info=True)
         return
+
 @firestore.transactional
 def update_in_transaction(transaction, news_ref,d):
     #import pdb; pdb.set_trace()
     news_to_write = [];
+    import pdb; pdb.set_trace()
     for key,value in d.items():
         doc_ref = news_ref.document(key);
         try:
@@ -142,17 +152,46 @@ def get_summary(details,stop_words):
             else:
                 summary=summary.replace("\n"," ")
     return summary
+
+def GenerateSitemap(newsList):
+    import pdb;pdb.set_trace()
+    root = ET.Element('urlset', attrib={"xmlns":"http://www.sitemaps.org/schemas/sitemap/0.9"})
+    date = datetime.now().strftime("%Y-%m-%d");
+    # home page
+    url = ET.SubElement(root, "url")
+    addValue(url, 'loc', 'https://www.quickakhbar.com')
+    addValue(url, "lastmod", "2021-04-01")
+    addValue(url, "changefreq", "monthly")
+    for news in newsList:
+        url = ET.SubElement(root, "url")
+        addValue(url, "loc", f'https://www.quickakhbar.com/{news["country"]}/{news["language"]}/{news["key"]}')
+        addValue(url, "lastmod", date)
+        addValue(url, "changefreq", "monthly")
+        tree = ET.ElementTree(root)
+        tree.write("./sitemap.xml")
+        bucket = firebase_admin.storage.bucket()
+        b = bucket.blob("sitemap.xml")
+        b.upload_from_filename("./sitemap.xml")
+        b.make_public()
+        x = requests.get('https://www.quickakhbar.com/sitemap.xml')
+                                                                                                    
+def addValue(parent, name, value):
+    child = ET.SubElement(parent, name)
+    child.text = value
         
 for data in news_data:
     array = []
+    sm_array = []
     tree = get_page_content(data["url"])
-    #import pdb;pdb.set_trace()
     node = tree.xpath(data["news_url_xpath"]) if tree else ""
     url_array = [ data["domain"] + x if "http" not in x else x for x in list(set(node))]
-    for i in url_array:
-        val = parse_news(i,data)
+    for i in url_array[0:3]:
+        val,s_map = parse_news(i,data)
         if val != None :
             array.append(val)
+            sm_array.append(s_map)
+    GenerateSitemap(sm_array)
+    #import pdb;pdb.set_trace()
     if not array:
         with open("/home/ubuntu/log.txt", 'a') as f:
             f.write("\n")
